7093d32c5d0790418b252071b7e1e85f5fd58c7b
akshaymore <akshaymore.ac@gmail.com>
2020-01-15 10:14:34 -0500

    Solved manifest.xml merge errors

--- app/src/main/java/moreakshay/com/tmdb/Test.kt
+++ app/src/main/java/moreakshay/com/tmdb/Test.kt
@@ -3,11 +3,6 @@ package moreakshay.com.tmdb
 class Test {
 
     fun test(a: Int?){
-        a.toString() //I am not forcing compiler to compile this line
-        a!!.toString() // I am forcing compiler to execute this line, and it may fail if a is null
-        // correct ?
-        a?.toString() //
-        // Kashyap
 
     }
 
---
+++ app/src/main/java/moreakshay/com/tmdb/home/DetailActivity.kt
@@ -0,0 +1,13 @@
+package moreakshay.com.tmdb.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import moreakshay.com.tmdb.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
